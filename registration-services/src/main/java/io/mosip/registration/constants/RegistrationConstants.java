954ecde0b114cd95542cd073b82a768a66d7c21c
Dinesh Kumar <gautam20995@gmail.com>
2020-12-22 16:25:11 +0530

    Ui Changes

--- registration-services/src/main/java/io/mosip/registration/constants/RegistrationConstants.java
+++ registration-services/src/main/java/io/mosip/registration/constants/RegistrationConstants.java
@@ -1178,7 +1178,7 @@ public class RegistrationConstants {
 	public static final String NUMBER_OR_NOTHING_REGEX = "^\\d*$";
 	public static final String FOUR_NUMBER_REGEX = "\\d{4}";
 	public static final String NUMBER_REGEX_ZERO_TO_THREE = "\\d{0,3}";
-	public static final int MOBILE_NUMBER_LENGTH = 9;
+	public static final int MOBILE_NUMBER_LENGTH = 12;
 	public static final String EMAIL_ID_REGEX = "^([\\w\\-\\.]+)@((\\[([0-9]{1,3}\\.){3}[0-9]{1,3}\\])|(([\\w\\-]+\\.)+)([a-zA-Z]{2,4}))$";
 	public static final String EMAIL_ID_REGEX_INITIAL = "([a-zA-Z]+\\.?\\-?\\@?(\\d+)?)+";
 	public static final String CNI_OR_PIN_NUMBER_REGEX = "\\d{0,30}";
